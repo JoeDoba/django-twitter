a65fa9ac1a17c80612939c88ccd0dbf1dc4abb2c
Joe <luqiao19930111@gmail.com>
2021-05-07 00:38:11 -0400

    add Tweet model

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -8,7 +8,6 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
-
 from accounts.api.serializers import UserSerializer, LoginSerializer, SignupSerializer
 
 
---
+++ tweets/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from utils.time_helpers import utc_now
+
+
+class Tweet(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def hours_to_now(self):
+        # datetime.now 不带时区信息，需要增加上 utc 的时区信息
+        return (utc_now() - self.created_at).seconds // 3600
+
+    def __str__(self):
+        return f'Created at: {self.created_at}\n' \
+               f'User name: {self.user}\n' \
+               f'Content: {self.content}\n'
